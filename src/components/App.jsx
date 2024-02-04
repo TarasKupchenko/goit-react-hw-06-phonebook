b967ce0211b3b02c1bc392d72dd75ee8fb89ee38
TarasKupchenko <133959505+TarasKupchenko@users.noreply.github.com>
2024-02-04 13:43:11 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-// App.jsx
 import React from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -5,15 +5,7 @@ const contactsSlice = createSlice({
   initialState: [],
   reducers: {
     addContact: (state, action) => {
-      const { name } = action.payload;
-
-      const isNameExists = state.some(contact => contact.name === name);
-
-      if (isNameExists) {
-        alert(`Contact with name ${name} already exists!`);
-      } else {
-        state.push(action.payload);
-      }
+      state.push(action.payload);
     },
     deleteContact: (state, action) => {
       return state.filter(contact => contact.id !== action.payload);
