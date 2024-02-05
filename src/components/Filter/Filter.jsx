aaf1c9bcd783d35ce0da6e1d88a212955e798622
TarasKupchenko <133959505+TarasKupchenko@users.noreply.github.com>
2024-02-05 13:19:44 +0200

    start

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilter } from '../../redux/filterSlice';
+import { selectFilter } from '../../redux/selectors';
 import css from './Filter.module.css';
 
 export const Filter = () => {
   const dispatch = useDispatch();
-  const filter = useSelector(state => state.filter);
+  const filter = useSelector(selectFilter);
 
   const handleChange = e => {
     dispatch(setFilter(e.target.value));
